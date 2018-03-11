8f408987fb961e88e7034b2ae63f10170efb33ba
Alexander Yashkin <alex.aspirine@gmail.com>
2018-03-11 11:31:15 +0300

    Use KISS and YAGNI

--- README.md
+++ README.md
@@ -21,11 +21,9 @@
 Для Ubuntu Trusty нужно сделать следующее:
 
 ```shell
-echo "deb http://ppa.launchpad.net/ubuntu-toolchain-r/test/ubuntu trusty main" | sudo tee -a /etc/apt/sources.list
 echo "deb http://hamsterisoft.bintray.com/apt-repo trusty main" | sudo tee -a /etc/apt/sources.list
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 1E9377A2BA9EF27F
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 379CE192D401AB61
-sudo apt-get update && sudo apt-get install libstdc++6 ip_filter
+sudo apt-get update && sudo apt-get install ip_filter
 ```
 
 И можно пользоваться:
@@ -49,29 +47,3 @@ cmake -H. -BBuild
 ```shell
 cmake --build .
 ```
-
-### Запуск тестов
-
-Запустить в каталоге Build
-
-```shell
-ctest -V
-```
-
-### Запуск теста производительности
-
-![bench result](https://github.com/ithamsteri/homework_02/raw/master/bench.png
-"Benchmark results")
-
-Для компиляции тестов производительности нужно установить
-[Google Benchmark](https://github.com/google/benchmark) библиотеку. Опция
-сборки тестов производительности по-умолчанию включена. Если CMake не найдет
-библиотеку Google Benchmark, то тогда тесты производительности не будут собраны
-и запущены.
-
-Тесты производительности запускаются совместно с модульными тестами командой:
-
-```shell
-ctest -V
-```
-
--- src/main.cpp
+++ src/main.cpp
@@ -2,26 +2,77 @@
 // File: main.cpp
 //
 
-#include "filter.h"
-#include "io.h"
-#include "ipaddr.h"
+#include <algorithm>
+#include <array>
+#include <cstddef>
 #include <iostream>
+#include <vector>
 
-int main(int, char *[]) {
-  try {
-    auto pool = readData(std::cin);
-    std::sort(pool.rbegin(), pool.rend());
-    print(std::cout, pool);
+using address_type = std::array<uint8_t, 4>;
+using pool_type = std::vector<address_type>;
 
-    auto result = filter(pool, 1);
-    print(std::cout, result);
+pool_type readData() {
+  pool_type pool;
 
-    result = filter(pool, 46, 70);
-    print(std::cout, result);
-
-    result = filter_any(pool, 46);
-    print(std::cout, result);
-  } catch (const std::exception &e) {
-    std::cerr << "Fatal Error: " << e.what() << std::endl;
+  for (std::string line; std::getline(std::cin, line);) {
+    auto start = line.cbegin();
+    address_type addr;
+    
+    for (auto i = address_type::size_type{0}; i < addr.size(); i++) {
+      auto iter = start;
+      while (iter != line.cend() && *iter != '.') iter++;
+      
+      auto value = std::stoi(std::string(start, iter++));
+      addr[i] = static_cast<uint8_t>(value);
+      start = iter;
+    }
+    
+    pool.push_back(addr);
   }
+
+  return pool;
+}
+
+std::ostream& operator<<(std::ostream& os, const address_type& addr)  
+{
+  for (auto iter = addr.cbegin(); iter != addr.cend(); iter++) {
+    if (iter != addr.cbegin()) os << '.';
+    os << static_cast<int>(*iter);
+  }
+  return os;  
+}  
+
+void filter(const pool_type& pool,
+            int n0,      int n1 = -1,
+            int n2 = -1, int n3 = -1) {
+  for(const auto& addr : pool) {
+    if (!(addr[0] != n0 ||
+         ((n1 > 0) && addr[1] != n1) ||
+         ((n2 > 0) && addr[2] != n2) ||
+         ((n3 > 0) && addr[3] != n3)))
+      std::cout << addr << std::endl;
+  }
+}
+
+void filter_any(const pool_type& pool, int n) {
+  for(const auto& addr : pool) {
+    for (auto i = address_type::size_type{0}; i < addr.size(); i++)
+      if (addr[i] == n) {
+        std::cout << addr << std::endl;
+        break;
+      }
+  }
+}
+
+int main(int, char* []) {
+  pool_type pool = readData();
+
+  std::sort(pool.rbegin(), pool.rend());
+
+  for (const auto& addr : pool)
+    std::cout << addr << std::endl;
+  
+  filter(pool, 1);
+  filter(pool, 46, 70);
+  filter_any(pool, 46);
 }
